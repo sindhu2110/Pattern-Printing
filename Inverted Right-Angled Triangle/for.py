17bd4fea5699b60b1481ed95726ace2090f84f55
Sindhu <nandisindhu2110@gmail.com>
2025-07-08 22:26:25 +0530

    Day 2: Inverted triangle pattern

---
+++ Inverted Right-Angled Triangle/for.py
@@ -0,0 +1,8 @@
+n= 5
+for row in range(n,0,-1):
+    for col in range(row):
+        print("*", end=" ")
+    print("")
+
+        
+    
---
+++ Inverted Right-Angled Triangle/while.py
@@ -0,0 +1,9 @@
+n=5
+row=n
+while row>0:        
+    col=0
+    while col<row:
+        print("*",end=" ")
+        col+=1
+    print("")
+    row-=1
