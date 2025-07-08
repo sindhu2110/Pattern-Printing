d0cd14ab9fb0e99b8fc9106134630a356ce5a46b
Sindhu <nandisindhu2110@gmail.com>
2025-07-08 21:38:40 +0530

    Day 1: Right-Angled Triangle

---
+++ while/while.py
@@ -0,0 +1,11 @@
+n = 5
+row=0
+
+while row<=n-1:      
+    print("")
+    row+=1
+    col = row
+    while col>=1:     
+        print("*", end="")        
+        col-=1
+    
---
+++ while/while1.py
@@ -0,0 +1,12 @@
+n = 5
+row=1
+
+while row<=n:      
+   
+    col = 1
+    while col<=row:     
+        print("*", end="")        
+        col+=1
+    print("")
+    row+=1
+    
