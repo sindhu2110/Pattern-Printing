992686a937a8e7596065908b795eaafab0f621bd
Sindhu <nandisindhu2110@gmail.com>
2025-07-10 15:56:25 +0530

    Day 3 - Right-Angled Triangle with Row Number Repetition

---
+++ Right-Angled Triangle with Row Number Repetition/for.py
@@ -0,0 +1,6 @@
+n = 5
+row = 0 
+for row in range(n):
+    for col in range(row+1):
+        print(row+1, end="" )
+    print()
---
+++ Right-Angled Triangle with Row Number Repetition/while.py
@@ -0,0 +1,10 @@
+n = 5
+row = 0
+while row<5:
+    #print(row, end=": ")
+    row+=1
+    col = 0
+    while col<row:
+        print(row, end=" ")
+        col+=1
+    print()
